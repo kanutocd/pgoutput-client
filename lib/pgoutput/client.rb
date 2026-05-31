8cb5158c90b5567ec14fd0732d4c0e3c15ec173a
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-05-31 18:29:06 +0800

    chore: initialize pgoutput-client gem structure

---
+++ lib/pgoutput/client.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "client/version"
+
+module Pgoutput
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pgoutput/client/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pgoutput
+  module Client
+    VERSION = "0.0.0"
+  end
+end
